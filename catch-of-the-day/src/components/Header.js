b9eac1341cb5e9e35ddd0bcdbcf6aa5554e6f9bf
Aral Tasher <novel@novel.local>
2018-11-27 12:38:26 -0500

    added firebase persistance for the menu

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -5,8 +5,8 @@ const Header = props => (
     <h1>
       Catch
       <span className="ofThe">
-        <span className="of">of</span>
-        <span className="the">the</span>
+        <span className="of">Of</span>
+        <span className="the">The</span>
       </span>
       Day
     </h1>
